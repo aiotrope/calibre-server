7751b3a4f95d35d09ad56ec2b74639980b74bf6b
aiotrope <arneli74@protonmail.com>
2023-01-05 20:42:29 +0200

    ♻️ refactor(api): edit files

--- public/schema/typeDefs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.typeDefs = void 0;
-var typeDefs = "#graphql\n    type User {\n        username: String!\n        id: ID!\n        successSignupMessage: String\n        repositories: [Repository]!\n    }\n\n    type Token {\n        token: String!\n        id: ID!\n        username: String!\n        successLoginMessage: String!\n    }\n\n    type Repository {\n        id: ID!\n        fullName: String!\n        description: String!\n        language: String!\n        forksCount: Int!\n        stargazersCount: Int!\n        ratingAverage: Float!\n        reviewCount: Int!\n        ownerAvatarUrl: String!\n        user: User!\n    }\n\n    input RepositoryInput {\n        fullName: String!\n        description: String\n        language: String\n        forksCount: Int\n        stargazersCount: Int\n        ratingAverage: Float\n        reviewCount: Int\n        ownerAvatarUrl: String\n    }\n\n    type Query {\n        users: [User]!\n        user(id: ID!): User!\n        repositories: [Repository!]!\n        repository(id: ID!): Repository!\n    }\n\n    type Mutation {\n        signup(username: String!, password: String!): User\n        login(username: String!, password: String!): Token\n        createRepository(repositoryInput: RepositoryInput): Repository\n    }\n   \n";
-exports.typeDefs = typeDefs;
